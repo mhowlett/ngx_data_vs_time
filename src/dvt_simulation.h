b1ab6e2346eaa17b576ca42d63ddf5abd84ca1c8
Matt Howlwett <matt@mhowlett.com>
2015-11-14 07:46:28 +0700

    added license text back into source files

--- src/dvt_simulation.h
+++ src/dvt_simulation.h
@@ -1,3 +1,21 @@
+/*
+ * ngx_data_vs_time
+ * (c) Matt Howlett 2015 https://www.matthowlett.com
+ *
+ * ngx_data_vs_time is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ngx_data_vs_time is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with ngx_data_vs_time.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __DVT_SIMULATION_H
 #define __DVT_SIMULATION_H
 
